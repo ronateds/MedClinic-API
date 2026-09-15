d6c22ea5911142161cb2df6f0c2d1a749a1eb261
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-09-14 23:25:50 -0300

    feat: RF02 – Configurar a conexão com o PostgreSQL utilizando TypeORM

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
-import express from "express";
+import 'reflect-metadata';
+import express from 'express';
+import { AppDataSource } from './database/data-source';
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
-});
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Data Source inicializado com sucesso!')
+
+        app.listen(3000, () => {
+            console.log('Servidor rodando na porta 3000');
+        })
+    }).catch((err) => {
+        console.error('Erro ao inicializar o Data Source', err);
+    })
