d9be1df70db6b8df62eab8a4c4af5ceaa322e38e
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-09-18 17:19:39 -0300

    feat: implementação dto

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { UsuarioController } from "../controllers/UsuarioController";
 import { asyncHandler } from "../middlewares/asyncHandler";
+import { validateDto } from "../middlewares/validate";
+import { CreateUsuarioDto } from "../dtos/CreateUsuarioDto";
 
 const usuarioRoutes = Router();
 
@@ -8,6 +10,7 @@ const usuarioController = new UsuarioController();
 
 usuarioRoutes.post(
     '/usuarios',
+    validateDto(CreateUsuarioDto),
     asyncHandler((req, res) => usuarioController.create(req, res))
 );
 
