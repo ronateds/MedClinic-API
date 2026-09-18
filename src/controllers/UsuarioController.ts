459d29d3cf6fa453bcd8c3b97468af9f358802dc
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-09-18 18:19:12 -0300

    refactor: logica de cadatro e implementação de hash de senha

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -1,15 +1,33 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../database/data-source";
 import { Usuario } from "../entities/Usuario";
+import { CreateUsuarioDto } from "../dtos/CreateUsuarioDto";
+import { AppError } from "../errors/AppError";
+import bcrypt from "bcryptjs"
 
+const usuarioRepository = AppDataSource.getRepository(Usuario);
 export class UsuarioController {
     async create(req: Request, res: Response): Promise<Response> {
-        const usuarioRepository = AppDataSource.getRepository(Usuario);
+        const { nome, email, senha, role }: CreateUsuarioDto = req.body;
 
-        const usuario = usuarioRepository.create(req.body);
+        const emailExite = await usuarioRepository.findOneBy({email});
+        if(emailExite) throw new AppError("Email já cadastrado", 400);
 
+        const senhaHash = await bcrypt.hash(senha, 10);
+
+        const usuario = usuarioRepository.create({
+            nome,
+            email,
+            senha: senhaHash,
+            role
+        });
         const savedUsuario = await usuarioRepository.save(usuario);
 
-        return res.status(201).json(savedUsuario);
+        return res.status(201).json({
+            id: savedUsuario.id,
+            nome: savedUsuario.nome,
+            email: savedUsuario.email,
+            role: savedUsuario.role
+        });
     }
 }
